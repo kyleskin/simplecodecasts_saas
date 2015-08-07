f9cfe1024362a1587160ff29d61f2120d8d0e563
Kyle Skinner <kyleskin@gmail.com>
2015-08-07 03:06:04 +0000

    Added validations and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,21 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  
   def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = "Message sent successfully! You're awesome!"
+      redirect_to new_contact_path
+    else
+      flash[:danger] = "Error Occurred. Oops!"
+      redirect_to new_contact_path
+    end
   end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
